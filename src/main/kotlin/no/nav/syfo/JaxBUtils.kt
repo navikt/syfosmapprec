5f8682a9755e1af6a9d550d738abe6f9f7e2776d
Joakim kartveit <joakim.kartveit@nav.no>
2018-11-19 09:47:17 +0100

    Byttet til xmlEIafelelsformat

--- src/main/kotlin/no/nav/syfo/JaxBUtils.kt
+++ src/main/kotlin/no/nav/syfo/JaxBUtils.kt
@@ -2,7 +2,6 @@ package no.nav.syfo
 
 import no.kith.xmlstds.apprec._2004_11_21.XMLAppRec
 import no.kith.xmlstds.msghead._2006_05_24.XMLMsgHead
-import no.nav.helse.sm2013.EIFellesformat
 import no.nav.model.sm2013.HelseOpplysningerArbeidsuforhet
 import no.trygdeetaten.xml.eiff._1.XMLEIFellesformat
 import no.trygdeetaten.xml.eiff._1.XMLMottakenhetBlokk
@@ -11,7 +10,7 @@ import javax.xml.bind.JAXBContext
 import javax.xml.bind.Marshaller
 import javax.xml.bind.Unmarshaller
 
-val fellesformatJaxBContext: JAXBContext = JAXBContext.newInstance(EIFellesformat::class.java, XMLMsgHead::class.java,
+val fellesformatJaxBContext: JAXBContext = JAXBContext.newInstance(XMLEIFellesformat::class.java, XMLMsgHead::class.java,
         XMLMottakenhetBlokk::class.java, HelseOpplysningerArbeidsuforhet::class.java, XMLAppRec::class.java)
 val fellesformatUnmarshaller: Unmarshaller = fellesformatJaxBContext.createUnmarshaller()
 
