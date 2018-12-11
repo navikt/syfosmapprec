7cadd22e4e9d47c8f0c9d57dd183323cd659120e
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-11 12:44:51 +0100

    Klar til prod setting

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -3,8 +3,6 @@ package no.nav.syfo
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "1").toInt(),
-    val srvappnameUsername: String = getEnvVar("SRVSYFOSMAPPREC_USERNAME", "username"),
-    val srvappnamePassword: String = getEnvVar("SRVSYFOSMAPPREC_PASSWORD", "password"),
     val mqHostname: String = getEnvVar("MQGATEWAY04_HOSTNAME"),
     val mqPort: Int = getEnvVar("MQGATEWAY04_PORT").toInt(),
     val mqGatewayName: String = getEnvVar("MQGATEWAY04_NAME"),
