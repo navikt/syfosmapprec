e4de4ef2ea58ab689558eafb587155acb03f6915
andreasnilsen <danduras@gmail.com>
2021-03-31 11:25:26 +0200

    Should send UTC timestamp

--- src/main/kotlin/no/nav/syfo/util/DateTime.kt
+++ src/main/kotlin/no/nav/syfo/util/DateTime.kt
@@ -1,7 +1,12 @@
 package no.nav.syfo.util
 
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 fun getDateTimeString(localDateTime: LocalDateTime): String {
     return localDateTime.toString()
+}
+
+fun getDateTimeString(timestamp: OffsetDateTime) : String {
+    return timestamp.toString()
 }
--- src/test/kotlin/no/nav/syfo/util/DateTimeKtTest.kt
+++ src/test/kotlin/no/nav/syfo/util/DateTimeKtTest.kt
@@ -4,10 +4,22 @@ import java.time.LocalDateTime
 import org.amshove.kluent.shouldEqual
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 
 class DateTimeKtTest : Spek({
     describe("Test datetime formating") {
-        val localDateTime = LocalDateTime.parse("2021-03-03T12:01:01")
-        getDateTimeString(localDateTime) shouldEqual "2021-03-03T12:01:01"
+        it("Get LocalDateTime correct") {
+            val localDateTime = LocalDateTime.parse("2021-03-03T12:01:01")
+            getDateTimeString(localDateTime) shouldEqual "2021-03-03T12:01:01"
+        }
+        it("Get OffsetDateTime correct") {
+            val offsetDateTime = OffsetDateTime.parse("2021-03-30T13:40:02+02:00")
+            getDateTimeString(offsetDateTime) shouldEqual "2021-03-30T13:40:02+02:00"
+        }
+        it("Get OffsetDateTime correct UTC") {
+            val offsetDateTime = OffsetDateTime.parse("2021-03-30T13:40:02Z")
+            getDateTimeString(offsetDateTime) shouldEqual "2021-03-30T13:40:02Z"
+        }
     }
 })
